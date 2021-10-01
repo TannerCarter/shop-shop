526e184949711ba58a08a235be7a64ce8076fd40
TannerCarter <tannerc.bootcamp@gmail.com>
2021-09-30 20:22:40 -0700

    cart-ui setup, added

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from "./reducers";
+import { useProductReducer } from './reducers'
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
@@ -7,16 +7,17 @@ const { Provider } = StoreContext;
 const StoreProvider = ({ value = [], ...props }) => {
   const [state, dispatch] = useProductReducer({
     products: [],
+    cart: [],
+    cartOpen: false,
     categories: [],
-    currentCategory: "",
+    currentCategory: '',
   });
-  // use this to confirm it works!
-  console.log(state);
+
   return <Provider value={[state, dispatch]} {...props} />;
 };
 
 const useStoreContext = () => {
   return useContext(StoreContext);
 };
-//export both functions
+
 export { StoreProvider, useStoreContext };
